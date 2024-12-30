8fce715a0bdf8c5d893c89f8913c03d50dc227d0
Sally Fung <sallyfung2018@gmail.com>
2024-12-30 21:42:57 +0800

    feat(admin portal): fine tune SideBar

--- src/components/admin_portal/SideBar/SideBar.tsx
+++ src/components/admin_portal/SideBar/SideBar.tsx
@@ -13,29 +13,36 @@ const SideBar: React.FC<SideBarProps> = () => {
 
     return (
         <div className='dashboard-sidebar'>
-            Dashboard
-            <ul className="menu menu-top">
-                <li className={location.pathname.startsWith('/dashboard') ? 'active' : ''}>
-                    <a href="/dashboard/profile">
-                        <FontAwesomeIcon icon={faIdCard} />
-                        <span>Profile</span>
-                    </a>
-                </li>
-                <li className={location.pathname.startsWith('/dashboard/enquiry') ? 'active' : ''}>
-                    <a href="/dashboard/enquiry">
-                        <FontAwesomeIcon icon={faPaperPlane} />
-                        <span>Enquiry</span>
-                    </a>
-                </li>
-            </ul>
-            <ul className="menu menu-bottom">
-                <li className='bottom'>
-                    <a href="/">
-                        <FontAwesomeIcon icon={faHouse} />
-                        <span>Website</span>
-                    </a>
-                </li>
-            </ul>
+            <div className='menu-top'>
+                <div className='menu-title'>
+                    Dashboard
+                </div>
+                <ul className="menu-item">
+                    <li className={location.pathname.startsWith('/dashboard') ? 'active' : ''}>
+                        <a href="/dashboard/profile">
+                            <FontAwesomeIcon icon={faIdCard} />
+                            <span>Profile</span>
+                        </a>
+                    </li>
+                    <li className={location.pathname.startsWith('/dashboard/enquiry') ? 'active' : ''}>
+                        <a href="/dashboard/enquiry">
+                            <FontAwesomeIcon icon={faPaperPlane} />
+                            <span>Enquiry</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className='menu-bottom'>
+                <ul className="menu-item">
+                    <li className='bottom'>
+                        <a href="/">
+                            <FontAwesomeIcon icon={faHouse} />
+                            <span>Website</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
         </div>
     );
 }
